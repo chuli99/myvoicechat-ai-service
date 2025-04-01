5adc9619c07f4640f2a4818d283ad0a80b466dda
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2025-03-31 22:22:10 -0300

    #2 Integrate F5-TTS for Basic Text-to-Speech

--- f5Tts.py
+++ f5Tts.py
@@ -5,7 +5,7 @@ COMANDO = [
     "--model", "F5TTS_v1_Base",
     "--ref_audio", "audios/audioJulian.ogg",
     "--ref_text", "Mientras mas corto es el audio, el modelo es mejor.",
-    "--gen_text", "Me and the devil. Walking side by side. And, though I have grown serene And strong since then, I think that God has willed A still renewable fear…"
+    "--gen_text", "Marge I am Brasilian guy. And, though I have grown serene And strong since then, I think that God has willed A still renewable fear…"
 ]
 #ref_text: Transcripción del audio de referencia
 #gen_text: Texto a sintetizar con la voz clonada
