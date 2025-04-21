dd429dfeaac142941219fdfc64dd3e7d7f83d04c
root <root@DESKTOP-8SPFL6V>
2025-04-21 11:15:19 -0300

    #3 Set up the basic project structure with FastAPI

--- f5Tts.py
+++ f5Tts.py
@@ -5,7 +5,7 @@ COMANDO = [
     "--model", "F5TTS_v1_Base",
     "--ref_audio", "audios/audioStefano.mp3",
     "--ref_text", "Mientras más corto es el audio, el modelo es mejor.",
-    "--gen_text", "Dealing with family secrets is never easy. Yet, sometimes, omission is a form of protection, intending to safeguard some from the harsh truths. One day, I hope you understand the reasons behind my actions. Until then, Anna, please, bear with me.",
+    "--gen_text", "这是一种策略，也是一种智慧。万变不离其宗，只要你把握住当前的游戏规则，就能玩转整个局势。“记住，预知敌情，了解自己，方能成就一番伟业。”我认真地对你说，希望你能明白这个道理。",
 ]
 
 resultado = subprocess.run(COMANDO, capture_output=True, text=True)
--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
+pip install fastapi uvicorn
 pip install f5-tts
-git clone https://github.com/SWivid/F5-TTS.git
-cd F5-TTS
-pip install -e .
-pip install torch==2.4.0+cu124 torchaudio==2.4.0+cu124 --extra-index-url https://download.pytorch.org/whl/cu124
+sudo apt update && sudo apt install ffmpeg -y
