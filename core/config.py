4037314d95ff2ea35141a348ff489e763cb236bb
chuli99 <juliancastillo0399@gmail.com>
2025-04-24 17:45:33 -0300

    #3 integrated F5-TTS libraries and API for best perfomance

--- core/config.py
+++ core/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     f5_tts_cmd: str = "f5-tts_infer-cli"
+    f5_tts_model_name: str = "F5TTS_v1_Base"  # Modelo predeterminado
+    use_gpu: bool = False
 
     class Config:
         env_file = ".env"
